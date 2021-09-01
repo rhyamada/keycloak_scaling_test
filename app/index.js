cfbf34d8c1e334cfec2dd9145e142f8be876f031
R Yamada <rhyamada@gmail.com>
2021-09-01 10:34:19 -0300

    ll

--- app/index.js
+++ app/index.js
@@ -7,19 +7,15 @@ const grant_type = 'authorization_code';
 const token_endpoint = 'http://ingress/auth/realms/example/protocol/openid-connect/token';
 const auth_endpoint = 'http://ingress/auth/realms/example/protocol/openid-connect/auth?client_id=client_id&response_type=code&scope=openid&redirect_uri=' + redirect_uri;
 
-let count = 0;
+let count = 0, N = 0, R = 0, T = 0, J = 0;
 let start = new Date().getTime();
-let next = 0;
-let N = 0;
-let R = 0;
-let T = 0;
-function time(){return new Date().getTime();}
-function cb(f,a,b){setTimeout(f,20000*Math.random(),a,b);}
+function time() { return new Date().getTime(); }
+function cb(f, a, b) { setTimeout(f, J * 100 * Math.random(), a, b); }
 function token(code) {
     let begin = time();
     needle.post(token_endpoint, { grant_type, client_id, client_secret, code, redirect_uri }, (err, res) => {
-        T += time()-begin;
-        R ++;
+        T += time() - begin;
+        R++;
         N--;
         if (err) {
             console.log("ERRO:" + err); return;
@@ -27,26 +23,17 @@ function token(code) {
         if (res.statusCode != 200) {
             console.log("ERRO:" + res.statusCode); return;
         }
-        count ++;
-        let now = time();
-        if(now>next){
-            let t = T/R;
-            console.log(N, count*1000/(now-start), T/R);
-            count = 0;
-            T = 0; R=0;
-            next = now+5000;
-            start = now;
-        }
+        count++;
     });
 }
 
 function form(url, Cookie) {
-    let username = 'user'+Math.floor(Math.random()*1000);
-    let data = 'username='+username+'&password=password';
+    let username = 'user' + Math.floor(Math.random() * 1000);
+    let data = 'username=' + username + '&password=password';
     let begin = time();
     needle.post(url, data, { headers: { Cookie } }, (err, res) => {
-        T += time()-begin;
-        R ++;
+        T += time() - begin;
+        R++;
         if (err) {
             N--;
             console.log("ERRO:" + err); return;
@@ -58,15 +45,15 @@ function form(url, Cookie) {
         let location = res.headers['location'];
         let a = location.indexOf('code=');
         let code = location.substring(a + 5);
-        cb(token,code);
+        cb(token, code);
     });
 }
 
 function auth(url) {
     let begin = time();
     needle.get(url, (err, res) => {
-        T += time()-begin;
-        R ++;
+        T += time() - begin;
+        R++;
         if (err) {
             N--;
             console.log("ERRO:" + err); return;
@@ -79,17 +66,25 @@ function auth(url) {
         let b = res.body.indexOf('"', a + 200);
         let url = res.body.substring(a + 8, b).replaceAll('&amp;', '&');
         let cookie = res.headers['set-cookie'].map((i) => { return i.split(';', 2)[0]; }).join('; ');
-        cb(form,url,cookie);
+        cb(form, url, cookie);
     });
 }
 
-let J = 1;
-function timer(){
-    if(J<90){J++;}
-    for(let i=0; i < J; i++){
+function timer() {
+    let now = time();
+    if (R > 0) {
+        let rate = Math.round(count * 1000 / (now - start));
+        let tr = Math.round(T / R);
+        T = 0; R = 0; count = 0; start = now;
+        console.log(N, rate, tr);
+    }
+    if (J < 200) {
+        J++;
+    }
+    for (let i = 0; i < J/2; i++) {
         N++;
-        cb(auth,auth_endpoint);
+        cb(auth, auth_endpoint);
     }
 }
 
-setInterval(timer,1000);
+setInterval(timer, 500);
